2c74346b06dab2360c492f8fd03755c700acc817
Y. Satotani <yoshikimethod@hotmail.co.jp>
2017-09-21 00:15:17 +0900

    amend dfs generator algorithm (diameter constraint)

--- source/naive_dfs/generator.cpp
+++ source/naive_dfs/generator.cpp
@@ -75,7 +75,10 @@ namespace ccheck {
   diameter_constraint(igraph_t* G) {
     igraph_integer_t diam;
     igraph_diameter(G, &diam, 0, 0, 0, 0, 1);
-    return (unsigned int)diam == m_config.Q() + 1;
+    if(m_config.R() > 0)
+      return (unsigned int)diam == m_config.Q() + 1;
+    else
+      return (unsigned int)diam == m_config.Q();
   }
 
   dfs_generator::dfs_generator() {
