e2284a209105bcd775b7888dff7b130c788cb797
Y. Satotani <yoshikimethod@hotmail.co.jp>
2017-09-25 18:37:26 +0900

    amend parameter generator

--- source/script/param_r2_experiment.py
+++ source/script/param_r2_experiment.py
@@ -2,17 +2,19 @@
 from __future__ import print_function
 from build_base_graph import *
 
-# xargs --max-args=6 time -o
+# xargs --max-args=1 sh -c
 
 print(
     ' '.join(map(
 
     lambda t:\
-        'n{n}.{prog}.time bin/{prog}.out -o result_r2/n{n}.{prog}.elist {n} 3'\
+        ('"time -p bin/{prog}.out ' +\
+         '-o result_r2/n{n}.{prog}.elist {n} 3 '+\
+         '2> result_r2/n{n}.{prog}.time"')\
     .format(n=n_node_btree(t[0], 3)+2, prog=t[1]),
 
     [(q, prog)
-     for q in range(2, 3)
+     for q in range(2, 6)
      for prog in ['naive_dfs', 'naive_dfs_r2']
     ]
 
