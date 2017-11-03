daf244d526065c50a8def3eb69b1fe763ec60c2d
Y. Satotani <yoshikimethod@hotmail.co.jp>
2017-11-03 15:52:45 +0900

    add result of minmaxmtr experiment

--- source/experiment/exp_cmp_minmax_mtr_param.py
+++ source/experiment/exp_cmp_minmax_mtr_param.py
@@ -4,12 +4,12 @@ from __future__ import print_function
 """
 (echo n,d,Q,R,sspl_lb,mgr,sspl,edge,node,time ;\
  ./exp_cmp_minmax_mtr_param.py |\
- xargs --max-lines=1 --max-procs=4 ./exp_cmp_minmax_mtr.out) >\
-exp_cmp_algo_result.csv
+ xargs --max-lines=1 --max-procs=3 ./exp_cmp_minmax_mtr.out) >\
+exp_cmp_minmax_mtr_result_2.csv
 """
 runs = 1
 d = 3
-N = list(range(4, 20, 2))
+N = list(range(4, 30, 2))
 
 print('\n'.join([
     '{} {} {}'.format(mgr, n, d)
