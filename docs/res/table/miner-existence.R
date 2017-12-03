203c6a49d6d4c5c7a077b7c3ae422defc7fd5781
Y. Satotani <yoshikimethod@hotmail.co.jp>
2017-12-03 13:22:07 +0900

    Update gmg existence figure

--- docs/res/table/miner-existence.R
+++ docs/res/table/miner-existence.R
@@ -6,19 +6,34 @@ library(ggplot2)
 data <- read_csv('miner.csv') %>%
   mutate(v = ifelse(sspl == sspl_lb, 1, 0)) %>%
   select(-c(Q, R, sspl_lb, sspl, edge, node, `time`)) %>%
-  mutate(n = as.factor(n), d = as.factor(d), v = as.factor(v))
+  mutate(v = as.factor(v))
 
 gp <- ggplot(
   data,
   aes(
     d,
-    ordered(n, rev(sort(unique(n)))),
+    #ordered(n, rev(sort(unique(n)))),
+    n,
     fill = v
     )
   ) +
   ggtitle('G-Moore graph existence') +
-  scale_x_discrete('Degree', position = 'top') +
-  scale_y_discrete('Order') +
+  geom_tile(
+    width = 0.9,
+    height = 0.9
+  ) +
+  scale_x_continuous(
+    'Degree',
+    position = 'top',
+    breaks = seq(min(data$d), max(data$d)),
+    expand = c(0, 0)
+    ) +
+  scale_y_continuous(
+    'Order',
+    breaks = seq(min(data$n), max(data$n)),
+    trans = 'reverse',
+    expand = c(0, 0)
+    ) +
   scale_fill_manual(
     name ='Exists?',
     values = c('grey50', 'coral'),
@@ -26,11 +41,12 @@ gp <- ggplot(
     breaks = c(1, 0),
     labels = c('yes', 'no')
     ) +
-  geom_tile(color = 'white') +
   theme(
     panel.grid = element_blank(),
+    panel.background = element_blank(),
+    plot.title = element_text(hjust = 0.5),
     axis.line = element_blank(),
     axis.ticks = element_blank()
     )
 ggsave('miner-existence.pdf', gp,
-       width = 8, height = 16, units = 'cm')
+       width = 7, height = 15, units = 'cm')
