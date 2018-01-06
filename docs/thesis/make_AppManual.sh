6e140d3f5421f44f1d81fe9b018f3f550bbfd3d1
Y. Satotani <yoshikimethod@hotmail.co.jp>
2018-01-06 16:45:40 +0900

    Update manual in thesis

--- docs/thesis/make_AppManual.sh
+++ docs/thesis/make_AppManual.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pandoc --chapters -o AppManual.tex ../../src/README.md
 sed -i s/section/section*/g AppManual.tex
+sed -i 's/ő/\\H{o}/g' AppManual.tex
+sed -i "s/é/{\\\'e\}/g" AppManual.tex
+sed -i "s/á/{\\\'a}/g" AppManual.tex
