d89afafdf9d997889df4a88aa864dc8160122e25
Y. Satotani <yoshikimethod@hotmail.co.jp>
2018-02-05 08:51:21 +0900

    Update minimum thesis

--- docs/res/plot-the/gmg-existence-h.R
+++ docs/res/plot-the/gmg-existence-h.R
@@ -7,7 +7,17 @@ theme_set(theme_light())
 
 data <- read_csv('../data/miner.csv') %>%
   mutate(v = ifelse(sspl == sspl_lb, 1, 0)) %>%
-  select(n, d, v) %>%
+  select(n, d, v)
+data <- data %>%
+  rbind(
+    expand.grid(
+      n = seq(min(data$n), max(data$n)),
+      d = seq(min(data$d), max(data$d)),
+      v = 2
+    ) %>%
+      filter(n > d, (n * d) %% 2 == 0)
+  ) %>%
+  distinct(n, d, .keep_all = TRUE) %>%
   mutate(v = as.factor(v))
 
 gp <- ggplot(
@@ -35,11 +45,10 @@ gp <- ggplot(
     expand = c(0, 0)
     ) +
   scale_fill_manual(
-    #name = 'Exists?',
     name = NULL,
-    values = c('grey50', 'royalblue'),
-    breaks = c(1, 0),
-    labels = c('存在する', '存在しない')
+    values = c('grey80', 'royalblue', 'skyblue'),
+    breaks = c(1, 0, 2),
+    labels = c('存在する', '存在しない', '探索未完了')
     ) +
   theme(
     text = element_text(size = 9),
